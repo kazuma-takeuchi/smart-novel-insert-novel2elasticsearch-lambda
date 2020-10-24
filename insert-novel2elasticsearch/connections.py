fe800954184bdec36847bf8f13dfbaaff46bf371
kazuma-takeuchi <kazuma12163098@gmail.com>
2020-10-24 20:42:29 +0900

    バグ修正

--- insert-novel2elasticsearch/connections.py
+++ insert-novel2elasticsearch/connections.py
@@ -11,15 +11,6 @@ service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, ES_REGION, service, session_token=credentials.token)
 
-# host = 'vpc-smartnovel-es-dev-j74tj7mufmh6cmogkjcudm6uq4.ap-northeast-1.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
-# region = 'ap-northeast-1' # e.g. us-west-1
-# service = 'es'
-# document = {
-#     "title": "testnovel",
-#     "author": "K.Takeuchi",
-#     "description": "this is test."
-# }
-# es.index(index="smart-novel", doc_type="_doc", body=document)
 
 def build_client(host=None):
     if host is None:
--- insert-novel2elasticsearch/lambda_functions.py
+++ insert-novel2elasticsearch/lambda_functions.py
@@ -5,29 +5,17 @@ from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 import boto3
 
-from connection import build_client
-from model import DEFAULT_BODY
+from connections import build_client
+from models import DEFAULT_DOCUMENT, NovelModel
 
 ES_INDEX = os.getenv("ES_INDEX")
 es = build_client()
 
 
-def create_document(event):
-    # TODO: 型チェック
-    document = DEFAULT_BODY
-    id_ = None
-    if 'id' in event:
-        id_ = event['id']
-    document = event['document']
-    if not isinstance(document['length'], int):
-        document['length'] = None
-    if 'like_count' in document and not isinstance(document['like_count'], int):
-        document['like_count'] = 0
-    return document, id_
-
-
 def lambda_handler(event, context):
-    document, id_ = create_document(event)
+    document = NovelModel(**event["document"]).dict()
+    id_ = event["id"]
+    print(f'id ={id_}, doc={document}')
     print('BEGIN insert')
     res = es.index(index=ES_INDEX, id=id_, body=document)
     print(res)
